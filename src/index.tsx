c3a285a3b187a738bfd20e99070c47c45047b154
JUNU <bokjunwoo@gmail.com>
2024-02-28 23:55:24 +0900

    feat: AppProvider 컴포넌트를 도입해 Provider를 관리

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline, createTheme } from '@mui/material';
+import React, { ReactNode } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ExpRateStateProvider } from './contexts/ExpRateStateProvider';
+import { LevelStateProvider } from './contexts/LevelStateProvider';
 
 const theme = createTheme({
   palette: {
@@ -13,7 +15,29 @@ const theme = createTheme({
   typography: {
     fontFamily: 'Noto Sans KR',
   },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 400,
+      md: 600,
+      lg: 1000,
+      xl: 1536,
+    },
+  },
 });
+
+type ContextType = React.ComponentType<{ children: ReactNode }>;
+
+interface AppProviderProps {
+  contexts: ContextType[];
+  children: ReactNode;
+}
+
+const AppProvider: React.FC<AppProviderProps> = ({ contexts, children }) => {
+  return contexts.reduce((prev, Context) => {
+    return React.createElement(Context, null, prev);
+  }, children) as React.ReactElement | null;
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -22,8 +46,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
+      <AppProvider contexts={[ExpRateStateProvider, LevelStateProvider]}>
+        <CssBaseline />
+        <App />
+      </AppProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
