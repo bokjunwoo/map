541b2aa9fcfc74f51db853472b203ef0ea040576
JUNU <bokjunwoo@gmail.com>
2024-02-28 23:59:58 +0900

    feat: App Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import AppLayout from './components/Layout/AppLayout';
+import SelectButtonGroupContainer from './components/SelectButtonGroup/SelectButtonGroupContainer';
+import UserLevelSearchInputContainer from './components/TextField/UserLevelSearchInputContainer';
+
 const App = () => {
-  return <div>프로젝트</div>;
+  return (
+    <AppLayout>
+      <SelectButtonGroupContainer
+        options={['직접선택', '불러오기']}
+        defaultOptionValue="직접선택"
+        localStorageKey="selectedOption"
+      />
+
+      <UserLevelSearchInputContainer />
+    </AppLayout>
+  );
 };
 
 export default App;
