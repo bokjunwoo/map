88265d68cb507b60c1a86a154594c7fb13090d07
JUNU <bokjunwoo@gmail.com>
2024-03-02 23:31:33 +0900

    feat: KinshipRing 컴포넌트 구현(혈맹의 반지)

---
+++ src/components/Exp/KinshipRing.tsx
@@ -0,0 +1,26 @@
+import useExpSelect from '../../hooks/useExpSelect';
+import { ExpSelectOption } from '../../interface/exp';
+import ExpSelectUI from './ExpSelectUI';
+
+const KinshipRing = () => {
+  const { expName, handleExpChange } = useExpSelect();
+
+  const expOptions: ExpSelectOption = {
+    label: '혈맹의 반지',
+    key: 'kinship_ring',
+    values: [
+      { value: 0, label: '미적용' },
+      { value: 10, label: '착용 (+10%)' },
+    ],
+  };
+
+  return (
+    <ExpSelectUI
+      options={expOptions}
+      value={expName}
+      handleChange={handleExpChange}
+    />
+  );
+};
+
+export default KinshipRing;
