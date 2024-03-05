1984ef7c4f42654a548e8530d96e45f351787524
JUNU <bokjunwoo@gmail.com>
2024-03-05 23:59:27 +0900

    feat: UnionPlacement 컴포넌트 구현(유니온 배치)

---
+++ src/components/Exp/UnionPlacement.tsx
@@ -0,0 +1,26 @@
+import { REGEX } from '../../constants/constants';
+import useExpInput from '../../hooks/useExpInput';
+import { ExpInputOption } from '../../interface/exp';
+import ExpInputUI from './ExpInputUI';
+
+const UnionPlacement = () => {
+  const { expName, handleExpChange } = useExpInput({
+    regex: REGEX.NUMBER_AND_DOT,
+    maxAllowedValue: 10,
+  });
+
+  const expOptions: ExpInputOption = {
+    label: '유니온 배치',
+    key: 'union_placement',
+  };
+
+  return (
+    <ExpInputUI
+      options={expOptions}
+      value={expName}
+      handleChange={handleExpChange}
+    />
+  );
+};
+
+export default UnionPlacement;
