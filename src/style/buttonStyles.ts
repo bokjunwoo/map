d4f5dac9dbb49dde2b9265e1127b85811e3da18b
JUNU <bokjunwoo@gmail.com>
2024-03-06 23:15:40 +0900

    feat: 버튼 스타일 구현

---
+++ src/style/buttonStyles.ts
@@ -0,0 +1,37 @@
+export const blackStyles = {
+  button: {
+    backgroundColor: 'transparent',
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+    color: '#333333',
+    border: '1px solid #333333',
+    borderRadius: '4px',
+  },
+  selectedButton: {
+    backgroundColor: '#333333',
+    '&:hover': {
+      backgroundColor: '#333333',
+    },
+    color: '#ffffff',
+  },
+};
+
+export const grayStyles = {
+  button: {
+    backgroundColor: 'transparent',
+    '&:hover': {
+      backgroundColor: 'transparent',
+    },
+    color: '#333333',
+    border: '1px solid #A2A2A2',
+    borderRadius: '4px',
+  },
+  selectedButton: {
+    backgroundColor: '#D3D3D3',
+    '&:hover': {
+      backgroundColor: '#D3D3D3',
+    },
+    color: '#333333',
+  },
+};
