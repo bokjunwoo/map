158cdcfad570434ea2a532742900752f649635b7
JUNU <bokjunwoo@gmail.com>
2024-03-12 23:57:00 +0900

    feat: RegionChip 컴포넌트 구현

---
+++ src/components/Chip/RegionChip.tsx
@@ -0,0 +1,29 @@
+import { useRecoilState } from 'recoil';
+import { regionListState } from '../../atoms/regionListState';
+import ImageChipUI from './UI/ImageChipUI';
+
+const RegionChip = ({ region }: { region: string }) => {
+  const [regionList, setRegionList] = useRecoilState(regionListState);
+
+  const isSelected = regionList.includes(region);
+
+  const toggleSelectedValue = () => {
+    setRegionList((prevDetailsName) => {
+      if (isSelected) {
+        return prevDetailsName.filter((value) => value !== region);
+      } else {
+        return [...prevDetailsName, region];
+      }
+    });
+  };
+
+  return (
+    <ImageChipUI
+      value={region}
+      isSelected={isSelected}
+      onToggle={toggleSelectedValue}
+    />
+  );
+};
+
+export default RegionChip;
