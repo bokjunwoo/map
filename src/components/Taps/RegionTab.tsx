8ce3d58c7f82ff4b6275c6fb3126cb71cc27d940
JUNU <bokjunwoo@gmail.com>
2024-03-12 23:58:05 +0900

    feat: RegionTab 컴포넌트 구현

--- src/components/Taps/RegionTab.tsx
+++ src/components/Taps/RegionTab.tsx
@@ -1,4 +1,6 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import { regionState } from '../../atoms/regionState';
 import { GRANDIS_REGION, ARCANERIVER_REGION } from '../../constants/constants';
 import { useLevelState } from '../../contexts/LevelStateProvider';
 import { Region } from '../../interface/data';
@@ -7,22 +9,20 @@ import RegionTabUI from './UI/RegionTabUI';
 const RegionTab = () => {
   const { level } = useLevelState();
 
-  const [value, setValue] = useState<Region>(
-    level >= 260 ? GRANDIS_REGION : ARCANERIVER_REGION
-  );
+  const [region, setRegion] = useRecoilState(regionState);
 
   const handleChange = (event: React.SyntheticEvent, newValue: Region) => {
-    setValue(newValue);
+    setRegion(newValue);
   };
 
   const regions = [ARCANERIVER_REGION, GRANDIS_REGION];
 
   useEffect(() => {
-    setValue(level >= 260 ? GRANDIS_REGION : ARCANERIVER_REGION);
-  }, [level]);
+    setRegion(level >= 260 ? GRANDIS_REGION : ARCANERIVER_REGION);
+  }, [level, setRegion]);
 
   return (
-    <RegionTabUI value={value} regions={regions} handleChange={handleChange} />
+    <RegionTabUI value={region} regions={regions} handleChange={handleChange} />
   );
 };
 
