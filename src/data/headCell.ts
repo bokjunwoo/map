6f66e6d8d06e16585f02bc145f7f0b54690b6182
JUNU <bokjunwoo@gmail.com>
2024-03-15 05:24:33 +0900

    feat: 선택한 시간에 따라 테이블 헤더 셀을 생성하는 함수 구현

---
+++ src/data/headCell.ts
@@ -0,0 +1,23 @@
+import { HeadCell } from '../interface/headCell';
+
+export const generateHeadCells: (selectedTime: string) => HeadCell[] = (
+  selectedTime
+) => [
+  { id: 'map_region', label: '지역', disable_sorting: true },
+  { id: 'map_name', label: '사냥터', disable_sorting: true },
+  { id: 'number_of_monsters', label: '몬스터 젠수' },
+  {
+    id: 'max_number_of_monster',
+    label: `${selectedTime} 최대 마릿수`,
+  },
+  {
+    id: 'max_experience_per_monster',
+    label: `${selectedTime} 최대 경험치`,
+  },
+  {
+    id: 'max_meso_per_monster',
+    label: `${selectedTime} 최대 순 메소`,
+    disableSorting: true,
+  },
+  { id: 'burning_field', label: '버닝필드', disableSorting: true },
+];
