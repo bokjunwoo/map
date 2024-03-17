75c8e4d82dee598a88c077d74436913a44dc7101
JUNU <bokjunwoo@gmail.com>
2024-03-17 23:55:42 +0900

    feat: calculateTotalMonstersWithTime 함수 구현

--- src/utils/calculate.ts
+++ src/utils/calculate.ts
@@ -1,3 +1,13 @@
 export const calculateSumOfMonsters = (numberOfMonsters: number[]) => {
   return numberOfMonsters.reduce((total, current) => total + current, 0);
 };
+
+export const calculateTotalMonstersWithTime = (
+  numberOfMonsters: number[],
+  time: number
+): number => {
+  return numberOfMonsters.reduce(
+    (total, current) => total + current * time * 8,
+    0
+  );
+};
