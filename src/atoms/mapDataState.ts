ae879eccdf6dd02e26e71aba76a3e33bc4adf32f
JUNU <bokjunwoo@gmail.com>
2024-03-18 23:59:21 +0900

    feat: 선택된 지도 데이터를 가져오는 셀렉터 생성

--- src/atoms/mapDataState.ts
+++ src/atoms/mapDataState.ts
@@ -1,8 +1,19 @@
-import { atom } from 'recoil';
+import { atom, selectorFamily } from 'recoil';
 import { mapData } from '../data/map';
 import { MapData } from '../interface/data';
 
 export const mapDataState = atom<MapData>({
   key: 'mapDataState',
   default: mapData,
+});
+
+export const getSelectedMapData = selectorFamily({
+  key: 'filteredMapDataSelector',
+  get:
+    (regions: (AraneRiverRegion | GrandisRegion)[]) =>
+    ({ get }) => {
+      const data = get(mapDataState);
+      const selectedData = regions.flatMap((region) => data[region]);
+      return selectedData;
+    },
 });
