84759b85588b66a3d79df7f3990cea8b6f84fc82
JUNU <bokjunwoo@gmail.com>
2024-03-20 22:43:47 +0900

    feat: MapDataRow 컴포넌트 구현

---
+++ src/components/Table/MapDataRow.tsx
@@ -0,0 +1,42 @@
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import { TableCell, TableRow, IconButton } from '@mui/material';
+import { useState } from 'react';
+import { symbolsColor } from '../../data/color';
+import { generateHeadCells } from '../../data/headCell';
+import { MapInfo } from '../../interface/data';
+import MapCellContent from './MapCellContent';
+
+interface MapDataRowProps {
+  item: MapInfo;
+  storedValue: string;
+}
+
+const MapDataRow = ({ item, storedValue }: MapDataRowProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    setIsOpen((prevOpen) => !prevOpen);
+  };
+
+  const headCells = generateHeadCells(storedValue);
+  const backgroundColor = symbolsColor[item.map_region].bgColor;
+
+  return (
+    <TableRow sx={{ bgcolor: backgroundColor }}>
+      <TableCell>
+        <IconButton onClick={toggleOpen}>
+          {isOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+        </IconButton>
+      </TableCell>
+
+      {headCells.map((headCell) => (
+        <TableCell key={headCell.id}>
+          <MapCellContent item={item} headCell={headCell} />
+        </TableCell>
+      ))}
+    </TableRow>
+  );
+};
+
+export default MapDataRow;
