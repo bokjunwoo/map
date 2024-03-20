d37eba19112a53d0106ab2a05f860006482f4598
JUNU <bokjunwoo@gmail.com>
2024-03-20 22:46:10 +0900

    feat: TableCellContentUI 컴포넌트 구현

---
+++ src/components/Table/UI/MapCellContentUI.tsx
@@ -0,0 +1,60 @@
+import { Box, SelectChangeEvent } from '@mui/material';
+import { MapInfo } from '../../../interface/data';
+import { HeadCell } from '../../../interface/headCell';
+import { SelectOptions } from '../../../interface/select';
+import { formatNumber } from '../../../utils/etc';
+import SelectCellUI from '../../common/SelectCellUI';
+
+interface TableCellContentUIProps {
+  headCell: HeadCell;
+  item: MapInfo;
+  selectedValue: number;
+  handleChange: (event: SelectChangeEvent<number>) => void;
+  selectOptions: SelectOptions;
+}
+
+const TableCellContentUI = ({
+  headCell,
+  item,
+  selectedValue,
+  handleChange,
+  selectOptions,
+}: TableCellContentUIProps) => {
+  switch (headCell.id) {
+    case 'burning_field':
+      return (
+        <SelectCellUI
+          value={selectedValue}
+          onChange={handleChange}
+          options={selectOptions}
+        />
+      );
+    case 'number_of_monsters':
+      return <Box>{item.number_of_monsters}</Box>;
+    case 'max_number_of_monster':
+      return <Box>{item.max_number_of_monster.toLocaleString()}</Box>;
+    case 'max_experience_per_monster':
+      return (
+        <Box>
+          {formatNumber(item.max_experience_per_monster)}
+          <br />
+          {item.max_experience_per_monster !== 0 &&
+            `(${item.max_experience_per_monster.toLocaleString()})`}
+        </Box>
+      );
+    case 'max_meso_per_monster':
+      return (
+        <Box>
+          {formatNumber(item.max_meso_per_monster)}
+          <br />
+          {item.max_meso_per_monster !== 0 &&
+            `(${item.max_meso_per_monster.toLocaleString()})`}
+        </Box>
+      );
+
+    default:
+      return <Box>{item[headCell.id]}</Box>;
+  }
+};
+
+export default TableCellContentUI;
