4829717637ecffc51184dd4474af08ea0a69b14c
JUNU <bokjunwoo@gmail.com>
2024-03-20 22:47:18 +0900

    feat: MapTable 컴포넌트 구현
    - 해당 컴포넌트는 테이블을 포함하고 있으며, 테이블의 정렬과 관련된 상태를 관리합니다.
    - MapSortHead 컴포넌트와 MapSortBody 컴포넌트를 사용하여 테이블을 구성합니다.

---
+++ src/components/Table/MapTable.tsx
@@ -0,0 +1,46 @@
+import { Paper, Table, TableContainer } from '@mui/material';
+import { useState } from 'react';
+import { useRecoilValue } from 'recoil';
+import { localStorageState } from '../../atoms/localStorageState';
+import { HeadCellInfo } from '../../interface/headCell';
+import MapSortBody from './MapSortBody';
+import MapSortHead from './MapSortHead';
+
+type MapTableContainerProps = {
+  localStorageKey: string;
+  defaultOptionValue: string;
+};
+
+const MapTable = ({
+  localStorageKey,
+  defaultOptionValue,
+}: MapTableContainerProps) => {
+  const storedValue = useRecoilValue(
+    localStorageState({ key: localStorageKey, defaultOptionValue })
+  );
+
+  const [order, setOrder] = useState<'asc' | 'desc'>('desc');
+  const [orderBy, setOrderBy] = useState<keyof HeadCellInfo>('map_name');
+
+  return (
+    <TableContainer component={Paper} sx={{ minWidth: 1100 }}>
+      <Table>
+        <MapSortHead
+          storedValue={storedValue}
+          order={order}
+          setOrder={setOrder}
+          orderBy={orderBy}
+          setOrderBy={setOrderBy}
+        />
+
+        <MapSortBody
+          storedValue={storedValue}
+          order={order}
+          orderBy={orderBy}
+        />
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default MapTable;
