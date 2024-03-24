720a223ac65426fa737b9b1f62dc228abda60b34
JUNU <bokjunwoo@gmail.com>
2024-03-24 23:50:12 +0900

    feat: formatNumber 단위 옵션 함수 구현

---
+++ src/utils/etc.ts
@@ -0,0 +1,23 @@
+export const formatNumber = (number: number, unit?: '메소' | '경험치') => {
+  if (number === 0) {
+    return '0';
+  }
+
+  const units = [' ', '만', '억', '조', '경'];
+  let result = '';
+  let unitIndex = 0;
+
+  while (number > 0) {
+    const remainder = number % 10000;
+    const chunk = remainder > 0 ? remainder + units[unitIndex] + ' ' : '';
+    result = chunk + result;
+    number = Math.floor(number / 10000);
+    unitIndex++;
+  }
+
+  if (unit) {
+    return result.trim() + ' ' + unit;
+  } else {
+    return result.trim();
+  }
+};
