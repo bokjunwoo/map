bf56f8d6a32333a88ce09cd30c7506055d6c41a0
JUNU <bokjunwoo@gmail.com>
2024-03-24 23:49:41 +0900

    refactor: calculateMesoMultiplier 함수 분리

--- src/utils/calculate.ts
+++ src/utils/calculate.ts
@@ -111,109 +111,117 @@ export const calculateMesoMultiplier = (
   playerLevel: number,
   monsters: MapMonsterInfo[]
 ): number[] => {
-  const multipliers: number[] = [];
+  const mesoMultipliers: number[] = [];
 
   for (const monster of monsters) {
-    const levelDifference = playerLevel - monster.level;
-
-    let multiplier: number;
-
-    if (levelDifference >= 30) {
-      multiplier = 1;
-    } else if (levelDifference >= 29) {
-      multiplier = 0.97;
-    } else if (levelDifference >= 28) {
-      multiplier = 0.84;
-    } else if (levelDifference >= 27) {
-      multiplier = 0.76;
-    } else if (levelDifference >= 26) {
-      multiplier = 0.65;
-    } else if (levelDifference >= 25) {
-      multiplier = 0.55;
-    } else if (levelDifference >= 24) {
-      multiplier = 0.46;
-    } else if (levelDifference >= 23) {
-      multiplier = 0.38;
-    } else if (levelDifference >= 22) {
-      multiplier = 0.31;
-    } else if (levelDifference >= 21) {
-      multiplier = 0.25;
-    } else if (levelDifference >= 20) {
-      multiplier = 0.2;
-    } else if (levelDifference >= 19) {
-      multiplier = 0.18;
-    } else if (levelDifference >= 18) {
-      multiplier = 0.16;
-    } else if (levelDifference >= 17) {
-      multiplier = 0.14;
-    } else if (levelDifference >= 16) {
-      multiplier = 0.12;
-    } else if (levelDifference >= 15) {
-      multiplier = 0.1;
-    } else if (levelDifference >= 14) {
-      multiplier = 0.08;
-    } else if (levelDifference >= 13) {
-      multiplier = 0.06;
-    } else if (levelDifference >= 12) {
-      multiplier = 0.04;
-    } else if (levelDifference >= 11) {
-      multiplier = 0.02;
-    } else if (levelDifference >= -10 && levelDifference <= 10) {
-      multiplier = 0;
-    } else if (levelDifference === -11) {
-      multiplier = 0.03;
-    } else if (levelDifference === -12) {
-      multiplier = 0.06;
-    } else if (levelDifference === -13) {
-      multiplier = 0.09;
-    } else if (levelDifference === -14) {
-      multiplier = 0.12;
-    } else if (levelDifference === -15) {
-      multiplier = 0.15;
-    } else if (levelDifference === -16) {
-      multiplier = 0.18;
-    } else if (levelDifference === -17) {
-      multiplier = 0.21;
-    } else if (levelDifference === -18) {
-      multiplier = 0.24;
-    } else if (levelDifference === -19) {
-      multiplier = 0.27;
-    } else if (levelDifference === -20) {
-      multiplier = 0.3;
-    } else if (levelDifference === -21) {
-      multiplier = 0.35;
-    } else if (levelDifference === -22) {
-      multiplier = 0.4;
-    } else if (levelDifference === -23) {
-      multiplier = 0.45;
-    } else if (levelDifference === -24) {
-      multiplier = 0.5;
-    } else if (levelDifference === -25) {
-      multiplier = 0.55;
-    } else if (levelDifference === -26) {
-      multiplier = 0.6;
-    } else if (levelDifference === -27) {
-      multiplier = 0.65;
-    } else if (levelDifference === -28) {
-      multiplier = 0.7;
-    } else if (levelDifference === -29) {
-      multiplier = 0.75;
-    } else if (levelDifference === -30) {
-      multiplier = 0.8;
-    } else if (levelDifference === -31) {
-      multiplier = 0.85;
-    } else if (levelDifference === -32) {
-      multiplier = 0.9;
-    } else if (levelDifference === -33) {
-      multiplier = 0.95;
-    } else {
-      multiplier = 1;
-    }
-
-    multipliers.push(1 - multiplier);
+    const multiplier = calculateIndividualMesoMultiplier(playerLevel, monster);
+    mesoMultipliers.push(multiplier);
   }
 
-  return multipliers;
+  return mesoMultipliers;
+};
+
+export const calculateIndividualMesoMultiplier = (
+  playerLevel: number,
+  monster: MapMonsterInfo
+): number => {
+  const levelDifference = playerLevel - monster.level;
+
+  let multiplier: number;
+
+  if (levelDifference >= 30) {
+    multiplier = 1;
+  } else if (levelDifference >= 29) {
+    multiplier = 0.97;
+  } else if (levelDifference >= 28) {
+    multiplier = 0.84;
+  } else if (levelDifference >= 27) {
+    multiplier = 0.76;
+  } else if (levelDifference >= 26) {
+    multiplier = 0.65;
+  } else if (levelDifference >= 25) {
+    multiplier = 0.55;
+  } else if (levelDifference >= 24) {
+    multiplier = 0.46;
+  } else if (levelDifference >= 23) {
+    multiplier = 0.38;
+  } else if (levelDifference >= 22) {
+    multiplier = 0.31;
+  } else if (levelDifference >= 21) {
+    multiplier = 0.25;
+  } else if (levelDifference >= 20) {
+    multiplier = 0.2;
+  } else if (levelDifference >= 19) {
+    multiplier = 0.18;
+  } else if (levelDifference >= 18) {
+    multiplier = 0.16;
+  } else if (levelDifference >= 17) {
+    multiplier = 0.14;
+  } else if (levelDifference >= 16) {
+    multiplier = 0.12;
+  } else if (levelDifference >= 15) {
+    multiplier = 0.1;
+  } else if (levelDifference >= 14) {
+    multiplier = 0.08;
+  } else if (levelDifference >= 13) {
+    multiplier = 0.06;
+  } else if (levelDifference >= 12) {
+    multiplier = 0.04;
+  } else if (levelDifference >= 11) {
+    multiplier = 0.02;
+  } else if (levelDifference >= -10 && levelDifference <= 10) {
+    multiplier = 0;
+  } else if (levelDifference === -11) {
+    multiplier = 0.03;
+  } else if (levelDifference === -12) {
+    multiplier = 0.06;
+  } else if (levelDifference === -13) {
+    multiplier = 0.09;
+  } else if (levelDifference === -14) {
+    multiplier = 0.12;
+  } else if (levelDifference === -15) {
+    multiplier = 0.15;
+  } else if (levelDifference === -16) {
+    multiplier = 0.18;
+  } else if (levelDifference === -17) {
+    multiplier = 0.21;
+  } else if (levelDifference === -18) {
+    multiplier = 0.24;
+  } else if (levelDifference === -19) {
+    multiplier = 0.27;
+  } else if (levelDifference === -20) {
+    multiplier = 0.3;
+  } else if (levelDifference === -21) {
+    multiplier = 0.35;
+  } else if (levelDifference === -22) {
+    multiplier = 0.4;
+  } else if (levelDifference === -23) {
+    multiplier = 0.45;
+  } else if (levelDifference === -24) {
+    multiplier = 0.5;
+  } else if (levelDifference === -25) {
+    multiplier = 0.55;
+  } else if (levelDifference === -26) {
+    multiplier = 0.6;
+  } else if (levelDifference === -27) {
+    multiplier = 0.65;
+  } else if (levelDifference === -28) {
+    multiplier = 0.7;
+  } else if (levelDifference === -29) {
+    multiplier = 0.75;
+  } else if (levelDifference === -30) {
+    multiplier = 0.8;
+  } else if (levelDifference === -31) {
+    multiplier = 0.85;
+  } else if (levelDifference === -32) {
+    multiplier = 0.9;
+  } else if (levelDifference === -33) {
+    multiplier = 0.95;
+  } else {
+    multiplier = 1;
+  }
+
+  return 1 - multiplier;
 };
 
 export const calculateTotalMeso: MesoCalculator = ({
