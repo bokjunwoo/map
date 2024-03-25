e6a7c5276d7942849d61fe655f6e6350028b6207
JUNU <bokjunwoo@gmail.com>
2024-03-25 23:43:52 +0900

    feat: MiniMapBodyUI 컴포넌트 구현

---
+++ src/components/Card/UI/MiniMapBodyUI.tsx
@@ -0,0 +1,20 @@
+import { CardMedia } from '@mui/material';
+import { MapInfo } from '../../../interface/map';
+
+const MiniMapBodyUI = ({ mapInfo }: { mapInfo: MapInfo }) => {
+  return (
+    <CardMedia
+      sx={{
+        borderTop: '4px solid #ffffff',
+        borderRadius: 1,
+        borderTopLeftRadius: '8px',
+        borderTopRightRadius: '8px',
+      }}
+      component="img"
+      image={require(`../../../assets/map/${mapInfo.map_region}/${mapInfo.map_name}.png`)}
+      alt={mapInfo.map_name}
+    />
+  );
+};
+
+export default MiniMapBodyUI;
