5fe594619440b72a6595621534c9e045ff918e80
JUNU <bokjunwoo@gmail.com>
2024-04-02 23:48:09 +0900

    feat: ListSubtitleUI 컴포넌트 구현

---
+++ src/components/common/ListSubtitleUI.tsx
@@ -0,0 +1,22 @@
+import { ListItem, ListItemText } from '@mui/material';
+
+const ListSubtitleUI = ({
+  title,
+  subtitle,
+}: {
+  title: string;
+  subtitle?: string;
+}) => {
+  return (
+    <ListItem>
+      <ListItemText
+        primary={title}
+        secondary={subtitle}
+        primaryTypographyProps={{ fontSize: 14 }}
+        secondaryTypographyProps={{ fontSize: 12 }}
+      ></ListItemText>
+    </ListItem>
+  );
+};
+
+export default ListSubtitleUI;
