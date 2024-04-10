56391482e04194f678ca8a65d1757dce3e6d99dc
JUNU <bokjunwoo@gmail.com>
2024-04-10 23:56:23 +0900

    feat: input value 상태관리 기능 구현

--- src/components/TextField/UI/InputAdornmentUI.tsx
+++ src/components/TextField/UI/InputAdornmentUI.tsx
@@ -1,67 +1,80 @@
 import { Box, InputAdornment, TextField, Typography } from '@mui/material';
+import { ChangeEvent } from 'react';
 
 type InputAdornmentUIProps = {
   text: string;
-  adornment: string;
+  adornment: number;
+
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  value: number;
 };
 
-const InputAdornmentUI = ({ text, adornment }: InputAdornmentUIProps) => {
-  <Box
-    sx={{
-      display: 'flex',
-      alignItems: 'center',
-    }}
-  >
-    <Typography
-      variant="body2"
+const InputAdornmentUI = ({
+  text,
+  adornment,
+  value,
+  onChange,
+}: InputAdornmentUIProps) => {
+  return (
+    <Box
       sx={{
-        pl: 1,
-        width: 200,
-        height: 35,
-        lineHeight: '35px',
-        borderTop: '1px solid #ddd',
-        borderLeft: '1px solid #ddd',
-        borderBottom: '1px solid #ddd',
-        borderTopLeftRadius: 4,
-        borderBottomLeftRadius: 4,
-        bgcolor: '#f5f5f5',
+        display: 'flex',
+        alignItems: 'center',
       }}
     >
-      {text}
-    </Typography>
+      <Typography
+        variant="body2"
+        sx={{
+          pl: 1,
+          width: 200,
+          height: 35,
+          lineHeight: '35px',
+          borderTop: '1px solid #ddd',
+          borderLeft: '1px solid #ddd',
+          borderBottom: '1px solid #ddd',
+          borderTopLeftRadius: 4,
+          borderBottomLeftRadius: 4,
+          bgcolor: '#f5f5f5',
+        }}
+      >
+        {text}
+      </Typography>
 
-    <TextField
-      size="small"
-      sx={{
-        width: 90,
-        '& .MuiOutlinedInput-root': {
-          '& fieldset': {
-            borderTopLeftRadius: 0,
-            borderBottomLeftRadius: 0,
-            borderColor: '#ddd',
+      <TextField
+        size="small"
+        value={value === 0 ? '' : value}
+        onChange={onChange}
+        sx={{
+          width: 90,
+          '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+              borderTopLeftRadius: 0,
+              borderBottomLeftRadius: 0,
+              borderColor: '#ddd',
+            },
+            '&:hover fieldset': {
+              borderColor: '#ddd',
+            },
+            '&.Mui-focused fieldset': {
+              border: '1px solid',
+              borderColor: '#FF9900',
+              boxShadow: `0 0 0 2px rgba(255, 153, 0, 0.4)`,
+            },
           },
-          '&:hover fieldset': {
-            borderColor: '#ddd',
-          },
-          '&.Mui-focused fieldset': {
-            border: '1px solid',
-            borderColor: '#FF9900',
-            boxShadow: `0 0 0 2px rgba(255, 153, 0, 0.4)`,
-          },
-        },
-      }}
-      InputProps={{
-        endAdornment: (
-          <InputAdornment position="end">
-            <Typography variant="body2">/ {adornment}</Typography>
-          </InputAdornment>
-        ),
-      }}
-      inputProps={{
-        sx: { textAlign: 'end', p: 0, fontSize: 14, height: 35 },
-      }}
-    />
-  </Box>;
+        }}
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <Typography variant="body2">/ {adornment}</Typography>
+            </InputAdornment>
+          ),
+        }}
+        inputProps={{
+          sx: { textAlign: 'end', p: 0, fontSize: 14, height: 35 },
+        }}
+      />
+    </Box>
+  );
 };
 
 export default InputAdornmentUI;
