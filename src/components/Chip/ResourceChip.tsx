5fc1c11aa53971081b93a9e8ebb657a8e80f2a09
JUNU <bokjunwoo@gmail.com>
2024-04-11 23:59:27 +0900

    refactor: resourceList를 props로 받아 사용하도록 변경

--- src/components/Chip/ResourceChip.tsx
+++ src/components/Chip/ResourceChip.tsx
@@ -1,18 +1,11 @@
 import { Stack } from '@mui/material';
-import { useExpRateValue } from '../../contexts/ExpRateStateProvider';
 import IconChipUI from './UI/IconChipUI';
 
-const ResourceChip = () => {
-  const expRate = useExpRateValue();
-  const mesoRate = 0;
-  const dropRate = 0;
-
-  const resourceList = [
-    { iconName: 'exp', label: expRate, color: '#bfff8a' },
-    { iconName: 'meso', label: mesoRate, color: '#ffec8a' },
-    { iconName: 'drop', label: dropRate, color: '#a1cbff' },
-  ];
-
+const ResourceChip = ({
+  resourceList,
+}: {
+  resourceList: ResourceListOption;
+}) => {
   return (
     <Stack direction="row" spacing={1}>
       {resourceList.map((resource) => (
