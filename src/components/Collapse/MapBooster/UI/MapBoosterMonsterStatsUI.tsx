0b87f5270bc3564c4bfda69f00dda6192a883d3a
JUNU <bokjunwoo@gmail.com>
2024-04-13 23:51:06 +0900

    feat: MapBoosterMonsterStatsUI 컴포넌트 구현

---
+++ src/components/Collapse/MapBooster/UI/MapBoosterMonsterStatsUI.tsx
@@ -0,0 +1,33 @@
+import { ListItem, ListItemText, Typography } from '@mui/material';
+import { MapMonsterInfo } from '../../../../interface/map';
+import ListSubheaderUI from '../../../common/ListSubheaderUI';
+import TextAndAmountLocaleStringUI from '../../../common/TextAndAmountLocaleStringUI';
+
+const MapBoosterMonsterStatsUI = ({ monster }: { monster: MapMonsterInfo }) => {
+  return (
+    <ListItem sx={{ display: 'block' }}>
+      <ListSubheaderUI title="몬스터 스탯" />
+
+      <ListItemText
+        primary={
+          <>
+            <Typography variant="body2" color="#d32f2f" fontSize={12}>
+              필드 최고렙 일반 몬스터 기본 경험치 적용, 레벨차이에 따른 경험치
+              배율 적용 X
+            </Typography>
+            <TextAndAmountLocaleStringUI
+              text="레벨(필드 최고렙 몬스터 레벨):"
+              amount={monster.level}
+            />
+            <TextAndAmountLocaleStringUI
+              text="경험치(필드 최고렙 몬스터 순수 경험치):"
+              amount={monster.experience}
+            />
+          </>
+        }
+      />
+    </ListItem>
+  );
+};
+
+export default MapBoosterMonsterStatsUI;
