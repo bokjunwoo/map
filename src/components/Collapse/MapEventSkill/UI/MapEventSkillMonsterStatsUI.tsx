6f254fe6072397b5c83822123a21006aa8430f78
JUNU <bokjunwoo@gmail.com>
2024-04-18 23:54:11 +0900

    feat: MapEventSkillMonsterStatsUI 컴포넌트 구현

---
+++ src/components/Collapse/MapEventSkill/UI/MapEventSkillMonsterStatsUI.tsx
@@ -0,0 +1,50 @@
+import { ListItem, ListItemText, Typography } from '@mui/material';
+import { MapMonsterInfo } from '../../../../interface/map';
+import ListSubheaderUI from '../../../common/ListSubheaderUI';
+import TextAndAmountLocaleStringUI from '../../../common/TextAndAmountLocaleStringUI';
+
+type MapEventSkillMonsterStatsUIProps = {
+  monster: MapMonsterInfo;
+  expMultiplier: number;
+  totalExpRate: number;
+};
+
+const MapEventSkillMonsterStatsUI = ({
+  monster,
+  expMultiplier,
+  totalExpRate,
+}: MapEventSkillMonsterStatsUIProps) => {
+  const expCalculate = expMultiplier * monster.experience * totalExpRate;
+
+  return (
+    <ListItem sx={{ display: 'block' }}>
+      <ListSubheaderUI title="스킬 설명" />
+      <ListItemText
+        primary={
+          <>
+            <Typography variant="body2" fontSize={14}>
+              스킬발동시 90마리 몬스터의 경험치 획득 / 경험치 도핑 적용 O
+            </Typography>
+            <Typography variant="body2" color="#d32f2f" fontSize={12}>
+              필드 최고렙 몬스터 경험치 적용, 캐릭터 레벨 비례 경험치 적용
+            </Typography>
+            <TextAndAmountLocaleStringUI
+              text="필드 최고렙 몬스터 레벨"
+              amount={monster.level}
+            />
+            <TextAndAmountLocaleStringUI
+              text="필드 최고렙 몬스터 순수 경험치"
+              amount={monster.experience * expMultiplier}
+            />
+            <TextAndAmountLocaleStringUI
+              text="배율이 적용된 경험치"
+              amount={expCalculate}
+            />
+          </>
+        }
+      />
+    </ListItem>
+  );
+};
+
+export default MapEventSkillMonsterStatsUI;
