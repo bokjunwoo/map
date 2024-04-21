01ab7ae7a086651914f0ef0ca43a436ea075dfee
JUNU <bokjunwoo@gmail.com>
2024-04-21 23:59:53 +0900

    refactor: MapBooster 컴포넌트 수정

--- src/components/Collapse/MapBooster.tsx
+++ src/components/Collapse/MapBooster.tsx
@@ -1,87 +1,23 @@
-import { SelectChangeEvent } from '@mui/material';
-import { useState, ChangeEvent } from 'react';
-import { REGEX } from '../../constants/constants';
-import { useExpRateValue } from '../../contexts/ExpRateStateProvider';
+import { useState } from 'react';
 import { MapInfo } from '../../interface/map';
-import { SelectOptions } from '../../interface/select';
 import MapBoosterUI from './UI/MapBoosterUI';
 
 const MapBooster = ({ item }: { item: MapInfo }) => {
-  const expRate = useExpRateValue();
-
   const highestLevelMonster = item.monsters.reduce((prev, curr) => {
     return prev.level > curr.level ? prev : curr;
   });
 
   const [numberOfMonster, setNumberOfMonster] = useState(180);
-  const [selectedBoosterValue, setSelectedBoosterValue] = useState(1);
-  const [selectedRuneValue, setSelectedRuneValue] = useState(0);
-  const [accumulationPotionPrice, setAccumulationPotionPrice] = useState(350);
-
-  const selectBoosterOptions: SelectOptions = {
-    label: '사용횟수',
-    values: Array.from({ length: 10 }, (_, i) => ({
-      value: i + 1,
-      label: `${i + 1}`,
-    })),
-  };
-
-  const selectedRuneOption: SelectOptions = {
-    label: '룬 설정',
-    values: [
-      { value: 0, label: '룬 X' },
-      { value: 100, label: '룬 2배' },
-      { value: 200, label: '룬 3배' },
-    ],
-  };
-
-  const handleNumberOfMonsterValue = (event: ChangeEvent<HTMLInputElement>) => {
-    const targetValue = Number(event.target.value);
-
-    if (REGEX.NUMBER.test(targetValue.toString())) {
-      setNumberOfMonster(targetValue);
-    }
-
-    if (targetValue > 180) {
-      setNumberOfMonster(180);
-    }
-  };
-
-  const handleSelectBoosterChange = (event: SelectChangeEvent<number>) => {
-    setSelectedBoosterValue(Number(event.target.value));
-  };
-
-  const handleSelectRuneChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setSelectedRuneValue(Number(event.target.value));
-  };
-
-  const handleAccumulationPotionPriceChange = (
-    event: ChangeEvent<HTMLInputElement>
-  ) => {
-    const targetValue = Number(event.target.value);
-
-    if (REGEX.NUMBER_UP_TO_THREE_DIGITS_REGEX.test(targetValue.toString())) {
-      setAccumulationPotionPrice(targetValue);
-    }
-  };
+  const [selectedRuneValue, setSelectedRuneValue] = useState(100);
 
   return (
     <MapBoosterUI
-      highestLevelMonster={highestLevelMonster}
-      selectBoosterOptions={selectBoosterOptions}
-      selectedRuneOption={selectedRuneOption}
-      selectedBoosterValue={selectedBoosterValue}
-      selectedRuneValue={selectedRuneValue}
-      numberOfMonster={numberOfMonster}
-      accumulationPotionPrice={accumulationPotionPrice}
-      handleNumberOfMonsterValue={handleNumberOfMonsterValue}
-      handleSelectBoosterChange={handleSelectBoosterChange}
-      handleSelectRuneChange={handleSelectRuneChange}
-      handleAccumulationPotionPriceChange={handleAccumulationPotionPriceChange}
       item={item}
-      expRate={expRate}
+      highestLevelMonster={highestLevelMonster}
+      selectedRuneValue={selectedRuneValue}
+      setSelectedRuneValue={setSelectedRuneValue}
+      numberOfMonster={numberOfMonster}
+      setNumberOfMonster={setNumberOfMonster}
     />
   );
 };
