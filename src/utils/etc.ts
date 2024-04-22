87fb1fec97e2427639b61cb557928623314e18b3
JUNU <bokjunwoo@gmail.com>
2024-04-22 23:53:08 +0900

    feat: convertToSeconds 함수 구현

--- src/utils/etc.ts
+++ src/utils/etc.ts
@@ -78,3 +78,26 @@ export const convertToFirstDecimal = (numberOfMonster: number) => {
     ((EVENT_SKILL.REQUIRED_MONSTER_COUNT / numberOfMonster) % 1) * 10
   );
 };
+
+export const convertToSeconds = (firstDecimal: number) => {
+  switch (firstDecimal) {
+    case 1:
+      return 1;
+    case 2:
+      return 2;
+    case 3:
+    case 4:
+      return 3;
+    case 5:
+      return 4;
+    case 6:
+    case 7:
+      return 5;
+    case 8:
+      return 6;
+    case 9:
+      return 7;
+    default:
+      return 0;
+  }
+};
