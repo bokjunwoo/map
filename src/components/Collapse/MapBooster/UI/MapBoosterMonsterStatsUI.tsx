404b95b341bb2aeda732fff49e9bd5a7496e459a
JUNU <bokjunwoo@gmail.com>
2024-04-23 23:57:17 +0900

    refactor: props 수정, TextAmountUI 컴포넌트를 사용하여 정보를 보여주도록 변경

--- src/components/Collapse/MapBooster/UI/MapBoosterMonsterStatsUI.tsx
+++ src/components/Collapse/MapBooster/UI/MapBoosterMonsterStatsUI.tsx
@@ -1,42 +1,51 @@
-import { ListItem, ListItemText, Typography } from '@mui/material';
+import { ListItem, ListItemText } from '@mui/material';
+import { useExpRateValue } from '../../../../contexts/ExpRateStateProvider';
 import { MapMonsterInfo } from '../../../../interface/map';
 import ListSubheaderUI from '../../../common/ListSubheaderUI';
-import TextAndAmountLocaleStringUI from '../../../common/TextAndAmountLocaleStringUI';
+import TextAmountUI from '../../../common/TextAmountUI';
 
 type MapBoosterMonsterStatsUIProps = {
   monster: MapMonsterInfo;
-  totalExpRate: number;
+  burningField: number;
+  runeValue: number;
 };
 
 const MapBoosterMonsterStatsUI = ({
   monster,
-  totalExpRate,
+  burningField,
+  runeValue,
 }: MapBoosterMonsterStatsUIProps) => {
-  const expCalculate = Math.floor(monster.experience * (totalExpRate / 100));
+  const expRate = useExpRateValue();
+
+  const totalExpRate = expRate + burningField + runeValue;
+
+  const expCalculate = Math.floor(
+    monster.experience * (totalExpRate / 100) * 10
+  );
 
   return (
     <ListItem sx={{ display: 'block' }}>
-      <ListSubheaderUI title="몬스터 스탯" />
+      <ListSubheaderUI
+        title="몬스터 스탯"
+        subtitle="필드 최고렙 몬스터 기준 | 필드 몬스터 순수 경험치 적용 | 경험치
+              배율 적용"
+        subtitleImportant={true}
+      />
 
       <ListItemText
         primary={
           <>
-            <Typography variant="body2" color="#d32f2f" fontSize={12}>
-              필드 최고렙 일반 몬스터 기본 경험치 적용, 레벨차이에 따른 경험치
-              배율 적용 X
-            </Typography>
-            <TextAndAmountLocaleStringUI
-              text="레벨(필드 최고렙 몬스터 레벨):"
+            <TextAmountUI
+              text="필드 최고렙 몬스터 레벨"
               amount={monster.level}
             />
-            <TextAndAmountLocaleStringUI
-              text="경험치(필드 최고렙 몬스터 순수 경험치):"
+
+            <TextAmountUI
+              text="필드 최고렙 몬스터 순수 경험치"
               amount={monster.experience}
             />
-            <TextAndAmountLocaleStringUI
-              text="경험치(경험치 배율이 적용된 경험치):"
-              amount={expCalculate}
-            />
+
+            <TextAmountUI text="배율이 적용된 경험치" amount={expCalculate} />
           </>
         }
       />
