c761cd2f62810c4fb0b3250c2acaecc3eace4d57
JUNU <bokjunwoo@gmail.com>
2024-04-23 19:11:16 +0900

    feat: subtitleImportant 기능 구현(subtitle 색상 변경)

--- src/components/common/ListSubheaderUI.tsx
+++ src/components/common/ListSubheaderUI.tsx
@@ -3,16 +3,21 @@ import { ListItemText } from '@mui/material';
 const ListSubheaderUI = ({
   title,
   subtitle,
+  subtitleImportant,
 }: {
-  title?: string;
+  title: string;
   subtitle?: string;
+  subtitleImportant?: boolean;
 }) => {
   return (
     <ListItemText
       primary={title}
       secondary={subtitle}
       primaryTypographyProps={{ fontSize: 16, fontWeight: 600 }}
-      secondaryTypographyProps={{ fontSize: 12 }}
+      secondaryTypographyProps={{
+        fontSize: 12,
+        color: subtitleImportant ? '#D32F2F' : 'text.secondary',
+      }}
     ></ListItemText>
   );
 };
