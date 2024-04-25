55fc9085637509fdfa3f059bcba9b363d487ff84
JUNU <bokjunwoo@gmail.com>
2024-04-25 23:56:55 +0900

    refactor: 부스터 관련 컴포넌트 구성으로 변경

--- src/components/Collapse/UI/MapBoosterUI.tsx
+++ src/components/Collapse/UI/MapBoosterUI.tsx
@@ -1,42 +1,30 @@
-import {
-  Box,
-  Divider,
-  List,
-  ListItem,
-  ListItemText,
-  SelectChangeEvent,
-  TextField,
-  Typography,
-} from '@mui/material';
-import { ChangeEvent } from 'react';
+import { Divider, List } from '@mui/material';
 import { MapInfo, MapMonsterInfo } from '../../../interface/map';
-import { SelectOptions } from '../../../interface/select';
-import AvatarChipUI from '../../common/AvatarChipUI';
-import DividerChipUI from '../../common/DividerChipUI';
-import SelectCellUI from '../../common/SelectCellUI';
-import TextAndAmountLocaleStringUI from '../../common/TextAndAmountLocaleStringUI';
-import MapCalculateRewardUI from './MapCalculateRewardUI';
+import LabelChipUI from '../../Chip/UI/LabelChipUI';
+import ListHeaderUI from '../../common/ListHeaderUI';
+import MapExpRateUI from '../../common/MapExpRateUI';
+import MapBoosterCalculateTable from '../MapBooster/MapBoosterCalculateTable';
+import MapBoosterCompare from '../MapBooster/MapBoosterCompare';
+import MapBoosterInput from '../MapBooster/MapBoosterInput';
+import MapBoosterExplanationUI from '../MapBooster/UI/MapBoosterExplanationUI';
+import MapBoosterMonsterStatsUI from '../MapBooster/UI/MapBoosterMonsterStatsUI';
 
 type MapBoosterUIProps = {
   highestLevelMonster: MapMonsterInfo;
-  selectOptions: SelectOptions;
-  selectedValue: number;
+  selectedRuneValue: number;
+  setSelectedRuneValue: React.Dispatch<React.SetStateAction<number>>;
   numberOfMonster: number;
-  handleSelectChange: (event: SelectChangeEvent<number>) => void;
-  handleNumberOfMonsterValue: (event: ChangeEvent<HTMLInputElement>) => void;
+  setNumberOfMonster: React.Dispatch<React.SetStateAction<number>>;
   item: MapInfo;
-  expRate: number;
 };
 
 const MapBoosterUI = ({
   highestLevelMonster,
-  selectOptions,
-  selectedValue,
+  selectedRuneValue,
+  setSelectedRuneValue,
   numberOfMonster,
-  handleSelectChange,
-  handleNumberOfMonsterValue,
+  setNumberOfMonster,
   item,
-  expRate,
 }: MapBoosterUIProps) => {
   return (
     <List
@@ -45,135 +33,55 @@ const MapBoosterUI = ({
       }}
       dense
     >
-      <AvatarChipUI
-        src="https://i.namu.wiki/i/DD0L-rzfOKMRj7b39GQA2JlWTdxM1RTJK5l90IBW5nWWt5MdoeicWit6QU1T9Ws7PYlnv0MsKctng8yyVOit7g.webp"
-        text="VIP 부스터 / 헤이스트"
-        label="이벤트 진행중 (03.28 ~ 04.24)"
+      <ListHeaderUI
+        iconPath={require(`../../../assets/etc_icon/booster.png`)}
+        headerText="VIP 부스터 / 헤이스트"
+        SecondaryComponent={
+          <LabelChipUI
+            label="이벤트 진행중 (03.28 ~ 04.24)"
+            rgb="255, 153, 0"
+          />
+        }
+        mainComment="캐릭터당 일일 10회 100초 동안 최대 180마리 처치 가능"
       />
 
       <Divider variant="middle" />
+      <MapBoosterExplanationUI />
 
-      <ListItem>
-        <ListItemText
-          primary={
-            <Typography variant="body2">
-              캐릭터당 일일 10회 100초 동안 최대 180마리 기준
-            </Typography>
-          }
-        ></ListItemText>
-      </ListItem>
-
-      <DividerChipUI label="몬스터 스탯" />
-
-      <ListItem>
-        <ListItemText
-          primary={
-            <>
-              <Typography variant="body2" color="#d32f2f">
-                필드 최고렙 일반 몬스터 기본 경험치 적용, 레벨차이에 따른 경험치
-                배율 적용 X
-              </Typography>
-              <TextAndAmountLocaleStringUI
-                text="레벨(필드 몬스터 레벨):"
-                amount={highestLevelMonster.level}
-              />
-              <TextAndAmountLocaleStringUI
-                text="경험치(필드 몬스터 순수 경험치):"
-                amount={highestLevelMonster.experience}
-              />
-            </>
-          }
-        />
-      </ListItem>
-
-      <DividerChipUI label="경험치 계산" />
-
-      <ListItem>
-        <Box sx={{ display: 'flex', alignItems: 'center' }}>
-          <SelectCellUI
-            options={selectOptions}
-            value={selectedValue}
-            onChange={handleSelectChange}
-          />
-
-          <Typography variant="body1" marginLeft={0.5}>
-            회
-          </Typography>
-        </Box>
-
-        <Box sx={{ display: 'flex', alignItems: 'center', ml: 0.5 }}>
-          <TextField
-            sx={{ width: 50 }}
-            id="outlined-size-small"
-            size="small"
-            value={numberOfMonster}
-            onChange={handleNumberOfMonsterValue}
-            inputProps={{ sx: { p: 0.5, textAlign: 'center' } }}
-          />
-
-          <Typography variant="body1" marginLeft={0.5}>
-            마리 기준
-          </Typography>
-        </Box>
-      </ListItem>
-
-      <MapCalculateRewardUI
-        label="1회 사용 경험치 획득량:"
-        rewardValue={
-          highestLevelMonster.experience *
-          numberOfMonster *
-          ((400 + item.burning_field) / 100)
-        }
-        rewardUnit="경험치"
+      <Divider variant="middle" />
+      <MapExpRateUI
+        burningField={item.burning_field}
+        rune={selectedRuneValue}
       />
 
-      <MapCalculateRewardUI
-        label={`총 사용(${selectedValue}회) 경험치 획득량:`}
-        rewardValue={
-          highestLevelMonster.experience *
-          numberOfMonster *
-          selectedValue *
-          ((400 + item.burning_field) / 100)
-        }
-        rewardUnit="경험치"
+      <Divider variant="middle" />
+      <MapBoosterMonsterStatsUI
+        monster={highestLevelMonster}
+        burningField={item.burning_field}
+        runeValue={selectedRuneValue}
       />
 
-      <DividerChipUI label="효율 계산" />
+      <Divider variant="middle" />
+      <MapBoosterInput
+        numberOfMonster={numberOfMonster}
+        setNumberOfMonster={setNumberOfMonster}
+      />
 
-      <ListItem>
-        <ListItemText
-          primary={
-            <>
-              <Typography variant="body1">
-                경험치 도핑 {expRate}% + 룬 {2}배 기준:
-              </Typography>
+      <Divider variant="middle" />
+      <MapBoosterCalculateTable
+        monsterExperience={highestLevelMonster.experience * 10}
+        burningField={item.burning_field}
+        setSelectedRuneValue={setSelectedRuneValue}
+      />
 
-              <Typography variant="body1">
-                1회 사용:{' '}
-                {(highestLevelMonster.experience *
-                  numberOfMonster *
-                  selectedValue *
-                  ((expRate + item.burning_field) / 100)) /
-                  highestLevelMonster.experience}
-                마리를 잡는 효과
-              </Typography>
-
-              <Typography variant="body1">
-                부스트가 약{' '}
-                {(
-                  (highestLevelMonster.experience *
-                    numberOfMonster *
-                    selectedValue *
-                    ((expRate + item.burning_field) / 100)) /
-                  highestLevelMonster.experience /
-                  (item.number_of_monster * 8)
-                ).toFixed(1)}
-                분의 효율을 가짐
-              </Typography>
-            </>
-          }
-        ></ListItemText>
-      </ListItem>
+      <Divider variant="middle" />
+      <MapBoosterCompare
+        burningField={item.burning_field}
+        runeValue={selectedRuneValue}
+        monsterExperience={highestLevelMonster.experience}
+        boosterKillNumberOfMonster={numberOfMonster}
+        FieldKillNumberOfMonster={item.number_of_monster}
+      />
     </List>
   );
 };
