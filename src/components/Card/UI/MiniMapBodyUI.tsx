825a893e4edd132341c89e5f05d13927b6c9aa41
JUNU <bokjunwoo@gmail.com>
2024-04-25 23:55:16 +0900

    style: maxWidth 추가

--- src/components/Card/UI/MiniMapBodyUI.tsx
+++ src/components/Card/UI/MiniMapBodyUI.tsx
@@ -9,6 +9,7 @@ const MiniMapBodyUI = ({ mapInfo }: { mapInfo: MapInfo }) => {
         borderRadius: 1,
         borderTopLeftRadius: '8px',
         borderTopRightRadius: '8px',
+        maxWidth: 400,
       }}
       component="img"
       image={require(`../../../assets/map/${mapInfo.map_region}/${mapInfo.map_name}.png`)}
