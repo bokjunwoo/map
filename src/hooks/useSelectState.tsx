cd014a9e3dd5704e4cc81b2c5f32015c768d7cd9
JUNU <bokjunwoo@gmail.com>
2024-04-26 23:47:43 +0900

    feat: useSelectState 커스텀 훅 구현

---
+++ src/hooks/useSelectState.tsx
@@ -0,0 +1,12 @@
+import { SelectChangeEvent } from '@mui/material';
+import { useState } from 'react';
+
+const useSelectState = (initialValue: number) => {
+  const [value, setValue] = useState<number>(initialValue);
+  const handleChange = (event: SelectChangeEvent) => {
+    setValue(Number(event.target.value));
+  };
+  return [value, handleChange] as const;
+};
+
+export default useSelectState;
