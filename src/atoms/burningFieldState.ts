24305f4aad13014e6c512f79e88f740326d1039f
JUNU <bokjunwoo@gmail.com>
2024-04-29 23:56:02 +0900

    feat: burningFieldState 셀렉터 함수 구현

--- src/atoms/burningFieldState.ts
+++ src/atoms/burningFieldState.ts
@@ -1,4 +1,4 @@
-import { atom } from 'recoil';
+import { atom, selectorFamily } from 'recoil';
 
 type BurningFieldItem = {
   map_name: string;
@@ -8,4 +8,15 @@ type BurningFieldItem = {
 export const burningFieldState = atom<BurningFieldItem[]>({
   key: 'burningFieldState',
   default: [],
+});
+
+export const getburningFieldValue = selectorFamily({
+  key: 'filteredBurningFieldValue',
+  get:
+    (mapName: string) =>
+    ({ get }) => {
+      const data = get(burningFieldState);
+      const item = data.find((item) => item.map_name === mapName);
+      return item ? item.burning_field : 0;
+    },
 });
