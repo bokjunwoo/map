33486087e25930b09f2a175436ce9547221bfc25
JUNU <bokjunwoo@gmail.com>
2024-04-29 23:57:06 +0900

    feat: MapSettingNumberOfMobUI 컴포넌트 구현

---
+++ src/components/Collapse/MapSetting/UI/MapSettingNumberOfMobUI.tsx
@@ -0,0 +1,16 @@
+import { ListItem } from '@mui/material';
+import { MapInfo } from '../../../../interface/map';
+import ListSubheaderUI from '../../../common/ListSubheaderUI';
+import NumberOfMonsterInput from '../../../TextField/NumberOfMonsterInput';
+
+const MapSettingNumberOfMobUI = ({ item }: { item: MapInfo }) => {
+  return (
+    <ListItem sx={{ display: 'block' }}>
+      <ListSubheaderUI title="마릿수 설정" />
+
+      <NumberOfMonsterInput text="1젠당 잡을 수 있는 마릿수" item={item} />
+    </ListItem>
+  );
+};
+
+export default MapSettingNumberOfMobUI;
