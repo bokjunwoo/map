cb71b5180e149728888d17d0bc30f1f5d9999e91
JUNU <bokjunwoo@gmail.com>
2024-04-29 23:56:53 +0900

    feat: MapSettingImageUI 컴포넌트 구현

---
+++ src/components/Collapse/MapSetting/UI/MapSettingImageUI.tsx
@@ -0,0 +1,15 @@
+import { Card } from '@mui/material';
+import { MapInfo } from '../../../../interface/map';
+import MiniMapBodyUI from '../../../Card/UI/MiniMapBodyUI';
+import MiniMapHeaderUI from '../../../Card/UI/MiniMapHeaderUI';
+
+const MapSettingImageUI = ({ item }: { item: MapInfo }) => {
+  return (
+    <Card sx={{ border: '4px solid #ffffff' }}>
+      <MiniMapHeaderUI mapInfo={item} />
+      <MiniMapBodyUI mapInfo={item} />
+    </Card>
+  );
+};
+
+export default MapSettingImageUI;
