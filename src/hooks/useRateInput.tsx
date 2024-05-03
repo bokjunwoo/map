25360cb7694489547328c70128cc58ce5704b2af
JUNU <bokjunwoo@gmail.com>
2024-05-03 22:10:14 +0900

    refactor: 변경된 함수에 따라 parms 수정

--- src/hooks/useRateInput.tsx
+++ src/hooks/useRateInput.tsx
@@ -1,13 +1,15 @@
 import { SelectChangeEvent } from '@mui/material';
 import { useState } from 'react';
-import { RecoilState, useSetRecoilState } from 'recoil';
+import { RecoilState, useRecoilState, useRecoilValue } from 'recoil';
+import { rateValueSelector } from '../atoms/expRateState';
+import { RateItem } from '../interface/rate';
 import { handleRateInputChange } from '../utils/hander';
 
 type UseRateInputProps = {
+  state: RecoilState<RateItem[]>;
+  rateName: string;
   regex: RegExp;
   maxAllowedValue: number;
-  state: RecoilState<number>;
-  initialValue?: string;
 };
 
 type UseRateInputResult = {
@@ -16,22 +18,25 @@ type UseRateInputResult = {
 };
 
 const useRateInput = ({
+  state,
+  rateName,
   regex,
   maxAllowedValue,
-  state,
-  initialValue,
 }: UseRateInputProps): UseRateInputResult => {
-  const setRateState = useSetRecoilState(state);
-  const [value, setValue] = useState<string>(initialValue || '');
+  const rateValue = useRecoilValue(rateValueSelector(rateName));
+
+  const [rate, setRate] = useRecoilState(state);
+  const [value, setValue] = useState<string>(rateValue);
 
   const handleRateChange = (event: SelectChangeEvent) => {
     const inputValue = event.target.value;
 
     handleRateInputChange({
-      inputValue: Number(inputValue),
-      currentValue: Number(value),
+      rateItem: rate,
+      rateName,
+      inputValue,
       setValue,
-      setRate: setRateState,
+      setRate,
       regex,
       maxAllowedValue,
     });
--- src/hooks/useRateSelect.tsx
+++ src/hooks/useRateSelect.tsx
@@ -1,28 +1,38 @@
 import { SelectChangeEvent } from '@mui/material';
 import { useState } from 'react';
-import { RecoilState, useSetRecoilState } from 'recoil';
+import { RecoilState, useRecoilState, useRecoilValue } from 'recoil';
+import { rateValueSelector } from '../atoms/expRateState';
+import { RateItem } from '../interface/rate';
 import { handleRateSelectChange } from '../utils/hander';
+
+type UseRateSelectProps = {
+  state: RecoilState<RateItem[]>;
+  rateName: string;
+};
 
 type UseRateSelectResult = {
   value: string;
   handleRateChange: (event: SelectChangeEvent) => void;
 };
 
-const useRateSelect = (
-  state: RecoilState<number>,
-  initialValue?: string
-): UseRateSelectResult => {
-  const setRateState = useSetRecoilState(state);
-  const [value, setValue] = useState<string>(initialValue || '');
+const useRateSelect = ({
+  state,
+  rateName,
+}: UseRateSelectProps): UseRateSelectResult => {
+  const rateValue = useRecoilValue(rateValueSelector(rateName));
+
+  const [rate, setRate] = useRecoilState(state);
+  const [value, setValue] = useState<string>(rateValue);
 
   const handleRateChange = (event: SelectChangeEvent) => {
     const inputValue = event.target.value;
 
     handleRateSelectChange({
-      inputValue: Number(inputValue),
-      currentValue: Number(value),
+      rateItem: rate,
+      rateName,
+      inputValue,
       setValue,
-      setRate: setRateState,
+      setRate,
     });
   };
 
