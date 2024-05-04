79689d26be3f455bbdd1265dbf718da33a3c17d3
JUNU <bokjunwoo@gmail.com>
2024-05-04 18:32:10 +0900

    feat: 새로운 props와 스타일을 적용

--- src/components/Chip/UI/IconChipUI.tsx
+++ src/components/Chip/UI/IconChipUI.tsx
@@ -1,34 +1,56 @@
-import { Chip, Avatar } from '@mui/material';
+import { Chip, Avatar, Box, Typography } from '@mui/material';
 
-const IconChipUI = ({
-  iconName,
-  label,
-  bgcolor,
-}: {
+type IconChipUIProps = {
   iconName: string;
-  label: number;
+  value: number;
+  text: string;
   bgcolor: string;
-}) => {
+};
+
+const IconChipUI = ({ iconName, value, text, bgcolor }: IconChipUIProps) => {
   return (
-    <Chip
-      component="span"
-      sx={{
-        fontSize: '0.7rem',
-        fontWeight: 600,
-        height: 20,
-        bgcolor,
-        borderRadius: 1,
-      }}
-      avatar={
-        <Avatar
-          alt={iconName}
-          src={require(`../../../assets/etc_icon/${iconName}.png`)}
-          variant="rounded"
-        />
-      }
-      label={`+ ${label}%`}
-      size="small"
-    />
+    <Box sx={{ width: 80 }}>
+      <Box
+        sx={{
+          textAlign: 'center',
+          bgcolor,
+          p: 0.5,
+          borderTopLeftRadius: 4,
+          borderTopRightRadius: 4,
+        }}
+      >
+        <Typography variant="body2" sx={{ fontSize: 12 }}>
+          {text}
+        </Typography>
+      </Box>
+
+      <Chip
+        component="span"
+        sx={{
+          display: 'flex',
+          fontSize: '0.7rem',
+          fontWeight: 600,
+          height: 25,
+          bgcolor: 'white',
+          border: `1px solid ${bgcolor}`,
+          borderRadius: 0,
+          borderBottomLeftRadius: 4,
+          borderBottomRightRadius: 4,
+          '.MuiChip-label': {
+            pr: '4px',
+          },
+        }}
+        avatar={
+          <Avatar
+            alt={iconName}
+            src={require(`../../../assets/etc_icon/${iconName}.png`)}
+            variant="rounded"
+          />
+        }
+        label={`+ ${value}%`}
+        size="small"
+      />
+    </Box>
   );
 };
 
