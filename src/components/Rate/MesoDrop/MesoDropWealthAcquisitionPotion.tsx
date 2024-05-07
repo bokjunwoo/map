b66051d9e024123af6c1fc2fc68fe1a6eb76e096
JUNU <bokjunwoo@gmail.com>
2024-05-07 23:02:31 +0900

    feat: 재획비 곱적용 %계산 기능 구현

--- src/components/Rate/MesoDrop/MesoDropWealthAcquisitionPotion.tsx
+++ src/components/Rate/MesoDrop/MesoDropWealthAcquisitionPotion.tsx
@@ -1,3 +1,4 @@
+import { useRecoilValue } from 'recoil';
 import { mesoDropState, rateValueSelector } from '../../../atoms/mesoDropState';
 import useRateSelect from '../../../hooks/useRateSelect';
 import { RateSelectOption } from '../../../interface/rate';
@@ -10,12 +11,35 @@ const MesoDropWealthAcquisitionPotion = () => {
     rateName: 'wealth_acquisition_potion',
   });
 
+  const mesoDropItem = useRecoilValue(mesoDropState);
+
+  const potionValue = () => {
+    const totalMesoDropRate = mesoDropItem.reduce(
+      (total, rate) => total + rate.value,
+      0
+    );
+    if (totalMesoDropRate === 0) return 20;
+
+    const potionRate = mesoDropItem.find(
+      (rate) => rate.rateName === 'wealth_acquisition_potion'
+    );
+
+    const isPotion = potionRate?.value !== 0 && potionRate;
+    const applyMultiplicationPotion = Math.floor(
+      (100 + totalMesoDropRate - (isPotion ? 20 : 0)) * 1.2 -
+        100 -
+        totalMesoDropRate
+    );
+
+    return applyMultiplicationPotion + (isPotion ? 20 : 0);
+  };
+
   const rateOption: RateSelectOption = {
     label: '재획비(곱적용)',
     key: 'wealth_acquisition_potion',
     values: [
       { value: 0, label: '미적용' },
-      { value: 20, label: '적용 (+20%)' },
+      { value: 20, label: `적용 (+${potionValue()})%` },
     ],
   };
 
