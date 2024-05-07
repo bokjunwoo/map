1633a83068bf6db7ae79d7a1ea4e6741e06bf0d5
JUNU <bokjunwoo@gmail.com>
2024-05-07 23:02:07 +0900

    feat: totalMesoDropSelector에 재획비 곱적용 로직 구현

--- src/atoms/mesoDropState.ts
+++ src/atoms/mesoDropState.ts
@@ -20,7 +20,23 @@ export const rateValueSelector = selectorFamily({
 export const totalMesoDropSelector = selector({
   key: 'totalMesoDropSelector',
   get: ({ get }) => {
-    const rates = get(mesoDropState);
-    return rates.reduce((total, rate) => total + rate.value, 0);
+    const mesoDropRate = get(mesoDropState);
+    const potionRate = mesoDropRate.find(
+      (rate) => rate.rateName === 'wealth_acquisition_potion'
+    );
+
+    if (potionRate?.value !== 0 && potionRate) {
+      const totalMesoDropRate = mesoDropRate.reduce(
+        (total, rate) => total + rate.value,
+        0
+      );
+      const applyMultiplicationPotion = Math.floor(
+        (100 + totalMesoDropRate - 20) * 1.2 - 100 - totalMesoDropRate
+      );
+
+      return totalMesoDropRate + applyMultiplicationPotion;
+    }
+
+    return mesoDropRate.reduce((total, rate) => total + rate.value, 0);
   },
 });
