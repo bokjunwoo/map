5379608611a15ae0ca5828521a25e6eabf82a40a
JUNU <bokjunwoo@gmail.com>
2024-05-08 23:57:59 +0900

    refactor: rateList 및 checkBoxList가 각각의 새로운 구조를 따르며, 이를 기반으로 컴포넌트 생성

---
+++ src/components/common/StackedListUI.tsx
@@ -0,0 +1,29 @@
+import { Stack } from '@mui/material';
+import { CheckBoxGroup, RateListOption } from '../../interface/stats';
+import RateChipUI from '../Chip/UI/RateChipUI';
+import CheckBoxListUI from './CheckBoxListUI';
+
+type StackedListUIProps = {
+  rateList?: RateListOption[];
+  checkBoxList?: CheckBoxGroup;
+};
+
+const StackedListUI = ({ rateList, checkBoxList }: StackedListUIProps) => {
+  return (
+    <Stack direction="row" spacing={1}>
+      {rateList?.map((option) => (
+        <RateChipUI key={option.text} rateList={option} />
+      ))}
+
+      {checkBoxList?.option.map((option) => (
+        <CheckBoxListUI
+          key={option.text}
+          checkBoxOption={option}
+          handleChange={checkBoxList.onChange}
+        />
+      ))}
+    </Stack>
+  );
+};
+
+export default StackedListUI;
