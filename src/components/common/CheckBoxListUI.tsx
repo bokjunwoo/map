0d948c24683ce1ac3d5bb9efd03419b31e88c306
JUNU <bokjunwoo@gmail.com>
2024-05-10 23:49:23 +0900

    feat: CheckBoxListUI 컴포넌트 구현

---
+++ src/components/common/CheckBoxListUI.tsx
@@ -0,0 +1,47 @@
+import { Box, Typography, Checkbox } from '@mui/material';
+import { CheckBoxOption } from '../../interface/stats';
+
+type CheckBoxListUIProps = {
+  checkBoxOption: CheckBoxOption;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const CheckBoxListUI = ({
+  checkBoxOption,
+  handleChange,
+}: CheckBoxListUIProps) => {
+  return (
+    <Box sx={{ width: 80 }}>
+      <Box
+        sx={{
+          textAlign: 'center',
+          bgcolor: checkBoxOption.bgcolor,
+          p: 0.5,
+          borderTopLeftRadius: 4,
+          borderTopRightRadius: 4,
+        }}
+      >
+        <Typography variant="body2" sx={{ fontSize: 12 }}>
+          {checkBoxOption.text}
+        </Typography>
+      </Box>
+
+      <Checkbox
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          height: 25,
+          border: `1px solid ${checkBoxOption.bgcolor}`,
+          borderRadius: 0,
+          borderBottomLeftRadius: 4,
+          borderBottomRightRadius: 4,
+        }}
+        checked={checkBoxOption.checked}
+        onChange={handleChange}
+        name={checkBoxOption.name}
+      />
+    </Box>
+  );
+};
+
+export default CheckBoxListUI;
