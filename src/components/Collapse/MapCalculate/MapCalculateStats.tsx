8254be1c2968f8eb6ef3f8900cac25bb431f4dbb
JUNU <bokjunwoo@gmail.com>
2024-05-10 23:50:32 +0900

    refactor: 새로운 타입 구조에 맞게 리스트 변경

--- src/components/Collapse/MapCalculate/MapCalculateStats.tsx
+++ src/components/Collapse/MapCalculate/MapCalculateStats.tsx
@@ -3,41 +3,42 @@ import { totalExpSelector } from '../../../atoms/expRateState';
 import { totalItemDropSelector } from '../../../atoms/itemDropState';
 import { totalMesoDropSelector } from '../../../atoms/mesoDropState';
 import { listRGB } from '../../../data/color';
-import MapCalculateRateUI from './UI/MapCalculateRateUI';
+import { RateListOption } from '../../../interface/stats';
+import MapStatsUI from '../common/MapStatsUI';
 
-const MapCalculateRate = ({ burningField }: { burningField: number }) => {
+const MapCalculateStats = ({ burningField }: { burningField: number }) => {
   const expRate = useRecoilValue(totalExpSelector);
   const ItemdropRate = useRecoilValue(totalItemDropSelector);
   const mesoDropRate = useRecoilValue(totalMesoDropSelector);
 
-  const resourceList: ResourceListOption = [
+  const rateList: RateListOption[] = [
     {
       iconName: 'exp',
       value: expRate,
       text: '경험치 획득량',
-      color: `rgba(${listRGB.blue}, 0.3)`,
+      bgcolor: `rgba(${listRGB.blue}, 0.3)`,
     },
     {
       iconName: 'burning',
       value: burningField,
       text: '버닝필드',
-      color: `rgba(${listRGB.red}, 0.6)`,
+      bgcolor: `rgba(${listRGB.red}, 0.6)`,
     },
     {
       iconName: 'drop',
       value: ItemdropRate,
       text: '아이템 드랍률',
-      color: `rgba(${listRGB.green}, 0.5)`,
+      bgcolor: `rgba(${listRGB.green}, 0.5)`,
     },
     {
       iconName: 'meso',
       value: mesoDropRate,
       text: '메소 획득량',
-      color: `rgba(${listRGB.orange}, 0.5)`,
+      bgcolor: `rgba(${listRGB.orange}, 0.5)`,
     },
   ];
 
-  return <MapCalculateRateUI resourceList={resourceList} />;
+  return <MapStatsUI rateList={rateList} />;
 };
 
-export default MapCalculateRate;
+export default MapCalculateStats;
