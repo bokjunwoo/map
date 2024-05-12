d8183d429d14045b78485a1a2fdce031bb9683c7
JUNU <bokjunwoo@gmail.com>
2024-05-12 18:05:16 +0900

    refactor: container컴포넌트 , UI 컴포넌트 구분

--- src/components/Collapse/MapPortal.tsx
+++ src/components/Collapse/MapPortal.tsx
@@ -1,14 +1,9 @@
-import { Divider, List } from '@mui/material';
 import { useState } from 'react';
 import { useRecoilValue } from 'recoil';
 import { userLevelState } from '../../atoms/userLevelState';
 import { MapInfo } from '../../interface/map';
 import { calculateIndividualExperienceMultiplier } from '../../utils/calculate';
-import ListHeaderUI from '../common/ListHeaderUI';
-import MapPortalStats from './MapPortal/MapPortalStats';
-import MapPortalPolloUI from './MapPortal/UI/MapPortalPolloUI';
-import MapPortalPrittoUI from './MapPortal/UI/MapPortalPrittoUI';
-import MapPortalWolfAndTotemUI from './MapPortal/UI/MapPortalWolfAndTotemUI';
+import MapPortalUI from './UI/MapPortalUI';
 
 type MapPortalProps = {
   item: MapInfo;
@@ -16,14 +11,14 @@ type MapPortalProps = {
 
 const MapPortal = ({ item }: MapPortalProps) => {
   const userLevel = useRecoilValue(userLevelState);
-  const [state, setState] = useState({
+  const [checkState, setCheckState] = useState({
     rune: false,
     sundayEvent: false,
   });
 
   const onChangeCheckBox = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setState({
-      ...state,
+    setCheckState({
+      ...checkState,
       [event.target.name]: event.target.checked,
     });
   };
@@ -38,41 +33,13 @@ const MapPortal = ({ item }: MapPortalProps) => {
   );
 
   return (
-    <List
-      sx={{
-        bgcolor: 'background.paper',
-      }}
-    >
-      <ListHeaderUI
-        iconPath={require('../../assets/etc_icon/Pollo.png')}
-        headerText="폴로 & 프리토"
-        mainComment="폴로 프리토의 효율에 대한 정보를 알 수 있습니다."
-      />
-
-      <Divider variant="middle" />
-      <MapPortalStats state={state} onChangeCheckBox={onChangeCheckBox} />
-
-      <Divider variant="middle" />
-      <MapPortalPolloUI
-        monsterExperience={highestLevelMonster.experience}
-        expMultiplier={expMultiplier}
-        mapName={item.map_name}
-      />
-
-      <Divider variant="middle" />
-      <MapPortalPrittoUI
-        monsterExperience={highestLevelMonster.experience}
-        expMultiplier={expMultiplier}
-        mapName={item.map_name}
-      />
-
-      <Divider variant="middle" />
-      <MapPortalWolfAndTotemUI
-        monsterExperience={highestLevelMonster.experience}
-        expMultiplier={expMultiplier}
-        mapName={item.map_name}
-      />
-    </List>
+    <MapPortalUI
+      mapName={item.map_name}
+      highestLevelMonster={highestLevelMonster}
+      checkState={checkState}
+      onChangeCheckBox={onChangeCheckBox}
+      expMultiplier={expMultiplier}
+    />
   );
 };
 
