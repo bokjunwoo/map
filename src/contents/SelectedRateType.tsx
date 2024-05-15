47e3be662c042b5fa621d3978a4b81b207e2e8c2
JUNU <bokjunwoo@gmail.com>
2024-05-15 18:18:57 +0900

    feat: SelectedRateOptionList 컴포넌트 구현

--- src/contents/SelectedRateType.tsx
+++ src/contents/SelectedRateType.tsx
@@ -4,6 +4,7 @@ import ExpList from '../components/Rate/List/ExpList';
 import ItemDropList from '../components/Rate/List/ItemDropList';
 import MesoDropList from '../components/Rate/List/MesoDropList';
 import SelectButtonGroupUI from '../components/SelectButtonGroup/UI/SelectButtonGroupUI';
+import RatesPanelUI from './RatesPanelUI';
 
 const SelectedRateOptionList = () => {
   const [selected, setSelected] = useState('경험치 획득량');
@@ -27,6 +28,8 @@ const SelectedRateOptionList = () => {
       {selected === '경험치 획득량' && <ExpList />}
       {selected === '아이템 드롭률' && <ItemDropList />}
       {selected === '메소 획득량' && <MesoDropList />}
+
+      <RatesPanelUI />
     </>
   );
 };
