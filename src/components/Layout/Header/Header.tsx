e5252ebea5127aac048536efc72cc302cf15740c
JUNU <bokjunwoo@gmail.com>
2024-05-22 23:52:24 +0900

    feat: 새소식 Link 이동 기능 구현

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,4 +1,5 @@
-import { AppBar, Container, Toolbar, Typography } from '@mui/material';
+import { AppBar, Box, Container, Toolbar, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
@@ -7,21 +8,44 @@ const Header = () => {
       sx={{ backgroundColor: '#f1f2f5', boxShadow: 0, mb: 3 }}
     >
       <Container>
-        <Toolbar sx={{ height: '90px' }} disableGutters>
+        <Toolbar sx={{ height: '70px' }} disableGutters>
           <Typography
+            variant="h1"
             component="a"
+            color="#FF9900"
+            fontWeight="bold"
             href="/"
             sx={{
-              pt: 2,
-              pb: 2,
-              fontWeight: 700,
-              fontSize: 40,
-              color: '#FF9900',
+              fontSize: 32,
               textDecoration: 'none',
             }}
           >
             버닝필드
           </Typography>
+          <Typography
+            color="#333333"
+            variant="body2"
+            fontWeight="bold"
+            sx={{ ml: 0.5, mt: 2 }}
+          >
+            BETA
+          </Typography>
+
+          <Box sx={{ ml: 5 }}>
+            <Link to="/news" style={{ textDecoration: 'none' }}>
+              <Typography
+                color="black"
+                fontWeight={500}
+                variant="h2"
+                sx={{
+                  fontSize: 16,
+                  textDecoration: 'none',
+                }}
+              >
+                새소식
+              </Typography>
+            </Link>
+          </Box>
         </Toolbar>
       </Container>
     </AppBar>
