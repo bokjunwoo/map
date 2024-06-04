e05684ec89925c623e487a9fc06c915f41a70681
JUNU <bokjunwoo@gmail.com>
2024-06-04 23:19:46 +0900

    fix: SelectChangeEvent<unknown> 변경

--- src/hooks/useSelectState.tsx
+++ src/hooks/useSelectState.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 
 const useSelectState = (initialValue: number) => {
   const [value, setValue] = useState<number>(initialValue);
-  const handleChange = (event: SelectChangeEvent) => {
+  const handleChange = (event: SelectChangeEvent<unknown>) => {
     setValue(Number(event.target.value));
   };
   return [value, handleChange] as const;
