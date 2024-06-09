ee7449dd4f7c3649989a564570a956a113981e30
JUNU <bokjunwoo@gmail.com>
2024-06-09 23:44:19 +0900

    feat: TableThCellUI 컴포넌트 구현(기본 th 구조)

---
+++ src/component/Table/common/TableThCellUI.tsx
@@ -0,0 +1,25 @@
+import { TableCell, TableCellProps } from '@mui/material';
+import { ReactNode } from 'react';
+
+type TableThCellUIProps = {
+  width?: string;
+  align?: TableCellProps['align'];
+  p?: number;
+  children?: ReactNode;
+};
+
+const TableThCellUI = ({ align, p, width, children }: TableThCellUIProps) => {
+  return (
+    <TableCell
+      component="th"
+      scope="row"
+      sx={{ bgcolor: '#F5F5F5', p: p }}
+      width={width}
+      align={align}
+    >
+      {children}
+    </TableCell>
+  );
+};
+
+export default TableThCellUI;
