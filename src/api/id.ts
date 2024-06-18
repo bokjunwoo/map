7f2d322b13957170aed524c7a44a7c35a71a761b
JUNU <bokjunwoo@gmail.com>
2024-06-18 18:23:04 +0900

    fix: getUserId 요청 주소 수정

--- src/api/id.ts
+++ src/api/id.ts
@@ -3,7 +3,7 @@ import { idApi } from './axiosConfig';
 // 유저 ocid
 export const getUserId = async (name: string) => {
   try {
-    const response = await idApi.get(`?character_name=${name}`);
+    const response = await idApi.get(`id?character_name=${name}`);
     return response.data.ocid;
   } catch (error) {
     console.error('Error fetching character:', error);
