2a392e4f0d2bfb183fd2185bd84a1597c71c55d6
JUNU <bokjunwoo@gmail.com>
2024-07-03 18:06:41 +0900

    refactor: usePortalBuffInfo 훅 사용으로 리팩토링

--- src/component/Collapse/MapPortal/section/MapPortalRateStatsSection.tsx
+++ src/component/Collapse/MapPortal/section/MapPortalRateStatsSection.tsx
@@ -1,14 +1,13 @@
-import { listRGB } from '../../../../data/color';
+import usePortalBuffInfo from '../../../../hooks/usePortalBuffInfo';
 import usePortalCheckInfo from '../../../../hooks/usePortalCheckInfo';
 import useRateInfo from '../../../../hooks/useRateInfo';
-import useUpDownButton from '../../../../hooks/useUpDownButton';
 import MapStatsUI from '../../../List/common/MapStatsUI';
 
 const MapPortalRateStatsSection = () => {
   const { onChangeCheckBox, checkRuneInfo, checkSundayEventInfo } =
     usePortalCheckInfo();
 
-  const { count, increment, decrement } = useUpDownButton(0);
+  const { buffButtonOption } = usePortalBuffInfo();
 
   const { expInfo } = useRateInfo({});
 
@@ -17,16 +16,6 @@ const MapPortalRateStatsSection = () => {
   const checkBoxList: CheckBoxGroup = {
     onChange: onChangeCheckBox,
     option: [checkRuneInfo, checkSundayEventInfo],
-  };
-
-  const buffButtonOption: BuffButtonOption = {
-    bgcolor: `rgba(${listRGB.orange}, 0.5)`,
-    text: '메디사의 축복',
-    count,
-    increment,
-    decrement,
-    disableIncrement: 5,
-    disableDecrement: 0,
   };
 
   return (
