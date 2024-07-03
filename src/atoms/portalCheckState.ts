c1967c421ecde595ee5cebf04be80844af2b6d79
JUNU <bokjunwoo@gmail.com>
2024-07-03 18:04:22 +0900

    feat: 포탈 룬, 선데이 경험치 비율을 위한 recoil 상태와 셀렉터 기능 추가

--- src/atoms/portalCheckState.ts
+++ src/atoms/portalCheckState.ts
@@ -1,9 +1,27 @@
-import { atom } from 'recoil';
+import { atom, selector } from 'recoil';
 
 export const portalCheckState = atom({
   key: 'portalCheckState',
   default: {
     rune: false,
     sundayEvent: false,
+  },
+});
+
+export const runeExpRateSelector = selector({
+  key: 'runeExpRateSelector',
+  get: ({ get }) => {
+    const portalBuffLevel = get(portalCheckState);
+    const expRate = portalBuffLevel.rune ? 100 : 0;
+    return expRate;
+  },
+});
+
+export const sundayEventExpRateRateSelector = selector({
+  key: 'sundayEventExpRateRateSelector',
+  get: ({ get }) => {
+    const portalBuffLevel = get(portalCheckState);
+    const expRate = portalBuffLevel.sundayEvent ? 100 : 0;
+    return expRate;
   },
 });
