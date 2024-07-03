c2395ead4ba4ca5eea1959205b01b3fdef9b4419
JUNU <bokjunwoo@gmail.com>
2024-07-03 18:03:38 +0900

    feat: 포탈 버프 경험치 비율을 위한 recoil 상태와 셀렉터 추가

---
+++ src/atoms/portalBuffState.ts
@@ -0,0 +1,16 @@
+import { atom, selector } from 'recoil';
+import { blessingPortalBuffExpRate } from '../utils/portal';
+
+export const portalBuffState = atom({
+  key: 'portalBuffState',
+  default: 0,
+});
+
+export const buffExpRateSelector = selector({
+  key: 'buffExpRateSelector',
+  get: ({ get }) => {
+    const portalBuffLevel = get(portalBuffState);
+    const expRate = blessingPortalBuffExpRate(portalBuffLevel);
+    return expRate;
+  },
+});
