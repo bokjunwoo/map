1ba2e80e0c059b9b99108d4f654b8a96f13eeea6
JUNU <bokjunwoo@gmail.com>
2024-07-04 18:16:40 +0900

    fix: CHARACTER_NAME 글자수 증가

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,7 +7,7 @@ export const REGEX = {
   NUMBER: /^[0-9]*$/,
   NUMBER_UP_TO_THREE_DIGITS_REGEX: /^[0-9]{1,3}$/,
   NUMBER_AND_DOT: /^\d*(\.\d{0,2})?$/,
-  CHARACTER_NAME: /^[A-Za-z0-9가-힣]{2,10}$/,
+  CHARACTER_NAME: /^[A-Za-z0-9가-힣]{2,12}$/,
 };
 
 export const GRANDIS_REGION = '그란디스';
