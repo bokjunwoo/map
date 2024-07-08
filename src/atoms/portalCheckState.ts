ea36eb7bb2e73652b5b6074aff12fb6a6f0909ee
JUNU <bokjunwoo@gmail.com>
2024-07-08 21:58:37 +0900

    feat: sundayEventExpEffectSelector 추가(값에 따라 경험치 배율을 3 또는 1로 설정)

--- src/atoms/portalCheckState.ts
+++ src/atoms/portalCheckState.ts
@@ -17,11 +17,20 @@ export const runeExpRateSelector = selector({
   },
 });
 
-export const sundayEventExpRateRateSelector = selector({
-  key: 'sundayEventExpRateRateSelector',
+export const sundayEventExpRateSelector = selector({
+  key: 'sundayEventExpRateSelector',
   get: ({ get }) => {
     const portalBuffLevel = get(portalCheckState);
     const expRate = portalBuffLevel.sundayEvent ? 200 : 0;
+    return expRate;
+  },
+});
+
+export const sundayEventExpEffectSelector = selector({
+  key: 'sundayEventExpEffectSelector',
+  get: ({ get }) => {
+    const portalBuffLevel = get(portalCheckState);
+    const expRate = portalBuffLevel.sundayEvent ? 3 : 1;
     return expRate;
   },
 });
