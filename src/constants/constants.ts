71ff07cf5b6a5f7cc8caafacdd58f90b22c6337d
JUNU <bokjunwoo@gmail.com>
2024-07-17 22:36:01 +0900

    update: EVENT_SKILL 업데이트

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -21,10 +21,10 @@ export const TIME_OPTIONS = [
 ];
 
 export const EVENT_SKILL = {
-  TITLE: '반 레온의 검술 - 신입기사',
+  TITLE: '반 레온의 검술 - 중급기사',
   SKILL_DESCRIPTION:
-    '최대 15마리의 적을 1500% 데미지로 6번 공격하는 폭발이 3회 발동되는 스킬이 1회 발동',
-  INITIAL_MOB_KILL: 135,
+    '최대 15마리의 적을 1800% 데미지로 6번 공격하는 스킬이 10회 발동',
+  INITIAL_MOB_KILL: 160,
   REQUIRED_MOB_COUNT: 300,
   DELAY: 8,
   ACTIVATION_COUNT: 30,
