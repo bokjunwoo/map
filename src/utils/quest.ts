47ca72b127f37338822e69354fa67b53988c7d18
JUNU <bokjunwoo@gmail.com>
2024-08-05 21:36:56 +0900

    feat: calculateEpicDungeonTotalExpPercentage 함수 구현

--- src/utils/quest.ts
+++ src/utils/quest.ts
@@ -2,6 +2,7 @@ import {
   araneRiverDailyQuestRegions,
   grandisDailyQuestRegions,
 } from '../data/quest';
+import { epicDungeonMinRegionsLevel } from '../data/region';
 import { calculateExpPercentage } from './calculate';
 
 export const calculateTotalExpPercentage = <T>({
@@ -25,10 +26,31 @@ export const calculateTotalExpPercentage = <T>({
   }, 0);
 };
 
-export const filterValidRegions = <T extends QuestRegion>(
+export const calculateEpicDungeonTotalExpPercentage = ({
+  characterLevel,
+  regions,
+  epicDungeonExp,
+}: CalculateEpicDungeonTotalExpPercentage): number => {
+  return regions.reduce((total, region) => {
+    const expReward = epicDungeonExp[region][characterLevel] || 0;
+    const isObtainable =
+      characterLevel >= (epicDungeonMinRegionsLevel[region] || 0);
+
+    if (isObtainable) {
+      const expPercentage = calculateExpPercentage({
+        userLevel: characterLevel,
+        expReward,
+      });
+      return total + expPercentage;
+    }
+    return total;
+  }, 0);
+};
+
+export const filterValidRegions = <T extends AllQuestRegion>(
   selectedRegions: T[],
   validRegions: T[]
-): T[] => {
+) => {
   return selectedRegions.filter((region) => validRegions.includes(region));
 };
 
